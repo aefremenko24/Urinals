fc449bc939bf8523bb6c3863a47a589ca103684b
Arthur Efremenko <aefremenko24@gmail.com>
2025-11-05 20:50:23 -0500

    Initial Commit

---
+++ UrinalsTests/UrinalsTests.swift
@@ -0,0 +1,17 @@
+//
+//  UrinalsTests.swift
+//  UrinalsTests
+//
+//  Created by Arthur Efremenko on 11/5/25.
+//
+
+import Testing
+@testable import Urinals
+
+struct UrinalsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
